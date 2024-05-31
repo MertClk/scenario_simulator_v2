c5a8b814674feca57083175b21a100fd9a307cc0
f0reachARR <f0reach@f0reach.me>
2024-05-31 17:15:13 +0900

    wip

--- openscenario/openscenario_interpreter/src/openscenario_interpreter.cpp
+++ openscenario/openscenario_interpreter/src/openscenario_interpreter.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ArduinoJson/Object/JsonObject.hpp>
 #include <chrono>
 #define OPENSCENARIO_INTERPRETER_NO_EXTENSION
 
